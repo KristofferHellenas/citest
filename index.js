e401bfa8d76d06e599edea27a852590b44b453f8
Kristoffer Hellenäs <kristoffer_hellenas@hotmail.se>
2020-04-19 13:13:39 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let greeting = function (a) {
+  return 'Hello ' + a + '!'
+}
+
+
+
+// En parameter och ett returvärde
+// Anpassa lösningen från den föregående uppgiften så att greeting har en parameter. Parametern kan antas innehålla en sträng.
+
+// Funktionen ska returnera resultatet av en strängsammanslagning involverande parametern så att “Hello”, följt av värdet i parametern, följt av “!”, visas.
+
+// Argumentet 'World' ska till exempel göra så att 'Hello World!' returneras.
